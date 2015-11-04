685ecc0aec2e75b131173166b0e16c76ec26b150
Mats Grimsgaard <mats@mgv.no>
2015-11-04 10:46:17 +0100

    Streamlined AdminHandler and enum_params

--- main.py
+++ main.py
@@ -24,16 +24,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 APPLICATION_YEAR = 2016
 
-def admin_required(handler):
-    if users.is_current_user_admin():
-        return True
-    else:
-        if users.get_current_user():
-            handler.abort(403)
-        else:
-            handler.redirect(users.create_login_url(handler.request.uri))
-        return False
-
 class LearningAssociation(ndb.Model):
     """Main model for representing learning associations."""
     name = ndb.StringProperty(required=True, default="")
@@ -93,6 +83,18 @@ class MainHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('application_form.html')
         self.response.write(template.render(template_values))
 
+    def enum_params(self, class_name, params, target):
+        classes = {
+            'previous_grants': PreviousGrants,
+            'other_grants': OtherGrants
+        }
+        for i, item in enumerate(self.request.POST.getall('_'.join([class_name,params[0]]))):
+            model = classes[class_name]()
+            setattr(model, params[0], item)
+            setattr(model, params[1], self.request.POST.getall('_'.join([class_name,params[1]]))[i])
+            if not model == classes[class_name]():
+                target.append(model)
+
     def get(self):
         grants_application = TravelGrantsApplication()
 
@@ -115,19 +117,8 @@ class MainHandler(webapp2.RequestHandler):
         if not learning_association:
             errors['learning_association'] = 'missing'
 
-        for i, item in enumerate(self.request.POST.getall('previous_grants_year')):
-            previous_grants = PreviousGrants()
-            previous_grants.year = item
-            previous_grants.location = self.request.POST.getall('previous_grants_location')[i]
-            if not previous_grants == PreviousGrants():
-                grants_application.previous_grants.append(previous_grants)
-
-        for i, item in enumerate(self.request.POST.getall('other_grants_provider')):
-            other_grants = OtherGrants()
-            other_grants.provider = item
-            other_grants.amount = self.request.POST.getall('other_grants_amount')[i]
-            if not other_grants == OtherGrants():
-                grants_application.other_grants.append(other_grants)
+        self.enum_params('previous_grants', ['year','location'], grants_application.previous_grants)
+        self.enum_params('other_grants', ['provider','amount'], grants_application.other_grants)
 
         if (len(errors) > 0):
             self.render_form(grants_application, errors)
@@ -226,30 +217,40 @@ Hilsen Voksenopplæringsforbundet
 
 
 class AdminHandler(webapp2.RequestHandler):
-    def get(self):
-        if admin_required(self):
-            if self.request.get('sf'):
-                is_new = self.request.get('sf') == 'new'
-                sf_id = self.request.get('sf').isdigit() and int(self.request.get('sf')) or self.request.get('sf')
-                learning_association = is_new and LearningAssociation() or LearningAssociation.get_by_id(sf_id)
-                learning_associations = None
+    def dispatch(self):
+        """This handler requires the current_user to be admin."""
+        if users.is_current_user_admin():
+            super(AdminHandler, self).dispatch()
+        else:
+            if users.get_current_user():
+                self.abort(403)
             else:
-                learning_association = None
-                learning_associations = LearningAssociation.query().order(LearningAssociation.name).fetch(50)
+                self.redirect(users.create_login_url(self.request.uri))
 
-            template_values = {
-                'application_year': APPLICATION_YEAR,
-                'learning_association': learning_association,
-                'learning_associations': learning_associations
-            }
+    def get_params(self):
+        return (self.request.get('sf') == 'new'), (self.request.get('sf').isdigit() and int(self.request.get('sf')) or self.request.get('sf'))
 
-            template = JINJA_ENVIRONMENT.get_template('admin_form.html')
-            self.response.write(template.render(template_values))
+    def get(self):
+        if self.request.get('sf'):
+            is_new, sf_id = self.get_params()
+            learning_association = is_new and LearningAssociation() or LearningAssociation.get_by_id(sf_id)
+            learning_associations = None
+        else:
+            learning_association = None
+            learning_associations = LearningAssociation.query().order(LearningAssociation.name).fetch(50)
+
+        template_values = {
+            'application_year': APPLICATION_YEAR,
+            'learning_association': learning_association,
+            'learning_associations': learning_associations
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('admin_form.html')
+        self.response.write(template.render(template_values))
 
     def post(self):
-        if admin_required(self) and self.request.get('sf'):
-            is_new = self.request.get('sf') == 'new'
-            sf_id = self.request.get('sf').isdigit() and int(self.request.get('sf')) or self.request.get('sf')
+        if self.request.get('sf'):
+            is_new, sf_id = self.get_params()
             learning_association = is_new and LearningAssociation() or LearningAssociation.get_by_id(sf_id)
             if learning_association:
                 learning_association.name = self.request.get('name')
